832656b22dc11ac940674d5ef15c9bfb649e4cb2
Mike Thielen <mike.thielen@gmail.com>
2015-03-17 23:46:49 -0500

    second commit

---
+++ desktop/src/com/barodapride/flappy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.barodapride.flappy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.barodapride.flappy.FlappyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyGame(), config);
+	}
+}
