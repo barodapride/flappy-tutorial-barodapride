4675c1a1ed95323822c41c31cd4790484a61296d
Mike Thielen <mike.thielen@gmail.com>
2015-03-20 19:17:27 -0500

    Added Assets class

---
+++ core/src/com/barodapride/flappy/Assets.java
@@ -0,0 +1,39 @@
+package com.barodapride.flappy;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by Mike on 3/20/2015.
+ */
+public class Assets {
+
+    // Disposeables
+    public static TextureAtlas atlas;
+    public static SpriteBatch batch;
+
+
+    // Non-Disposeables
+    public static TextureRegion bird;
+
+    public static void load() {
+
+        atlas = new TextureAtlas("pack.atlas");
+        batch = new SpriteBatch();
+
+        bird = atlas.findRegion("bird");
+
+    }
+
+    // Dispose all disposeables here
+    public static void dispose(){
+        if (atlas != null) {
+            atlas.dispose();
+        }
+        if (batch != null) {
+            batch.dispose();
+        }
+    }
+
+}
